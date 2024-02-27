93ad876940912fb3176c0905d3c641142597d16e
GusEngers <gustavoengers6441@gmail.com>
2024-02-27 17:32:15 -0300

    Modelo Email

---
+++ src/api/models/email.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require('mongoose');
+
+const Email = model(
+  'email',
+  new Schema({
+    project: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    variables: [
+      {
+        type: String,
+      },
+    ],
+    message: {
+      type: String,
+      required: true,
+    },
+  })
+);
+
+module.exports = Email;
