759e2c612ad7ea6d9ccbb7bb6dda1c20fa8ec7f8
GusEngers <gustavoengers6441@gmail.com>
2024-02-28 17:03:52 -0300

    FIX: Añadir autorización por OAuth

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,13 +1,21 @@
 const nodemailer = require('nodemailer');
-const { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } = require('../utils/constants');
+const {
+  NODEMAILER_EMAIL,
+  NODEMAILER_PASSWORD,
+  NODEMAILER_OAUTH_CLIENTID,
+  NODEMAILER_OAUTH_CLIENT_SECRET,
+  NODEMAILER_OAUTH_REFRESH_TOKEN,
+} = require('../utils/constants');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: NODEMAILER_EMAIL,
     pass: NODEMAILER_PASSWORD,
+    clientId: NODEMAILER_OAUTH_CLIENTID,
+    clientSecret: NODEMAILER_OAUTH_CLIENT_SECRET,
+    refreshToken: NODEMAILER_OAUTH_REFRESH_TOKEN,
   },
 });
 
